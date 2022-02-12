d19c782632d1c587e1ace7b86742f055c87edfa6
Андрей Берлин <99011598+Andrey-Berlin@users.noreply.github.com>
2022-02-12 21:12:15 +0200

    update script.js

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-let shotit = function() {
-    html2canvas(document.getElementById('section'), {
-        onrendered: function(canvas) {
-            document.body.appendChild(canvas);
-            leCanvas = document.getElementsByTagName("canvas")[0];
-            let img = leCanvas.toDataURL("image/png");
-            document.write('<br><br><span style="font:14px normal Helvetica, Arial; font-weight: bold; color:#13a6f5; margin-left:16px">Это Ваш скриншот. Нажав, можете сохранить как картинку. Чтобы вернуться, обновите страницу браузера:</span> <br><br> <img src="' + img + '"/>');
-        },
-        /*width: 390,
-        height: 220*/
-    });
-}
 
 
 
